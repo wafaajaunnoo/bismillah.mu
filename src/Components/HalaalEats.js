08517c8e47db23e6eeb691b79dc7c0ce50edc687
Wafaa Jaunnoo <wafaajaunnoo@gmail.com>
2024-03-26 22:56:15 +0400

    [ADD] List

--- src/Components/HalaalEats.js
+++ src/Components/HalaalEats.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import restaurants from './restaurants.json'
+import Fetcheddata from './Fetcheddata';
 
 const HalaalEats = () => {
   return (
-    <div>HalaalEats</div>
+    <div>
+      <Fetcheddata data={restaurants}/>
+    </div>
+    
   )
 }
 
