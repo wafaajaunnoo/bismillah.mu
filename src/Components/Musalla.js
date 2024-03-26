60ab2822806f202e0de6edcbafd9c947918c722f
Wafaa Jaunnoo <wafaajaunnoo@gmail.com>
2024-03-26 22:55:05 +0400

    [FIX] Headings

--- src/Components/Musalla.js
+++ src/Components/Musalla.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Musalla = () => {
   return (
-    <div>Musalla</div>
+    <div>
+      <h1>Serene spaces for your prayers.</h1>
+      <h3>We've meticulously sourced a variety of mosques and prayer rooms for you to pray in.</h3>
+    </div>
   )
 }
 
